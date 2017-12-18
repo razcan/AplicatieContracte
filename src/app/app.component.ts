44b70ac95ac2eb688e2ad95249887e53b908c824
Razvan Mustata <razvan.mustata@gmail.com>
2017-12-18 10:43:44 +0200

    setup proeict bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+// import $ = require('jquery')
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'app';
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+
+import { ButtonsModule,BsDropdownModule,BsDatepickerModule,AlertModule } from 'ngx-bootstrap';
+
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
