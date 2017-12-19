92e400b1ef645a53451415a5193482673698dea5
Razvan Mustata <razvan.mustata@gmail.com>
2017-12-19 15:24:39 +0200

    emitere factura

---
+++ src/app/invoice-module/purchase-invoice/purchase-invoice.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
+
+@Component({
+  selector: 'app-purchase-invoice',
+  templateUrl: './purchase-invoice.component.html',
+  styleUrls: ['./purchase-invoice.component.css']
+})
+export class PurchaseInvoiceComponent implements OnInit {
+  // colorTheme = 'theme-dark-blue';
+  minDate = new Date(2017, 5, 10);
+  maxDate = new Date(2018, 9, 15);
+  _bsValue1: Date;
+  _bsValue2: Date;
+  constructor() { }
+  get bsValue1(): Date {
+    return this._bsValue1;
+  }
+  set bsValue1(v1: Date) {
+    console.log(v1);
+    this._bsValue1 = v1;
+  }
+  get bsValue2(): Date {
+    return this._bsValue2;
+  }
+  set bsValue2(v2: Date) {
+    console.log(v2);
+    this._bsValue2 = v2;
+  }
+  colorTheme = 'theme-green';
+
+  ngOnInit() {
+    this.bsConfig = Object.assign({}, { containerClass: this.colorTheme });
+  }
+
+  bsConfig: Partial<BsDatepickerConfig>;
+ 
+  }
+
