dd7cbc611ab0c5bd8eeba04016a9bd159cabd39e
Razvan Mustata <razvan.mustata@gmail.com>
2017-12-19 22:36:49 +0200

    added dragble and sortable objects

--- src/app/invoice-module/purchase-invoice/purchase-invoice.component.ts
+++ src/app/invoice-module/purchase-invoice/purchase-invoice.component.ts
@@ -9,26 +9,39 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class PurchaseInvoiceComponent implements OnInit {
   // colorTheme = 'theme-dark-blue';
-  minDate = new Date(2017, 5, 10);
-  maxDate = new Date(2018, 9, 15);
-  _bsValue1: Date;
-  _bsValue2: Date;
-  constructor() { }
-  get bsValue1(): Date {
-    return this._bsValue1;
-  }
-  set bsValue1(v1: Date) {
-    console.log(v1);
-    this._bsValue1 = v1;
-  }
-  get bsValue2(): Date {
-    return this._bsValue2;
-  }
-  set bsValue2(v2: Date) {
-    console.log(v2);
-    this._bsValue2 = v2;
-  }
+  // minDate = new Date(2017, 5, 10);
+  // maxDate = new Date(2018, 9, 15);
+  // _bsValue1: Date;
+  // _bsValue2: Date;
+  // constructor() { }
+  // get bsValue1(): Date {
+  //   return this._bsValue1;
+  // }
+  // set bsValue1(v1: Date) {
+  //   console.log(v1);
+  //   this._bsValue1 = v1;
+  // }
+  // get bsValue2(): Date {
+  //   return this._bsValue2;
+  // }
+  // set bsValue2(v2: Date) {
+  //   console.log(v2);
+  //   this._bsValue2 = v2;
+  // }
   colorTheme = 'theme-green';
+
+  itemObjectsLeft: any[] = [
+    { id: 1, name: 'Windstorm' },
+    { id: 2, name: 'Bombasto' },
+    { id: 3, name: 'Magneta' }
+  ];
+ 
+  itemObjectsRight: any[] = [
+    { id: 4, name: 'Tornado' },
+    { id: 5, name: 'Mr. O' },
+    { id: 6, name: 'Tomato' }
+  ];
+
 
   ngOnInit() {
     this.bsConfig = Object.assign({}, { containerClass: this.colorTheme });
