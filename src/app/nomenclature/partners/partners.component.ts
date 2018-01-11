762907c648052d90edfb5214a8c78dcec0d37fb0
Razvan Mustata <razvan.mustata@gmail.com>
2018-01-11 18:34:47 +0200

    Rezolvare tabel lista parteneri

--- src/app/nomenclature/partners/partners.component.ts
+++ src/app/nomenclature/partners/partners.component.ts
@@ -48,7 +48,6 @@ constructor (private http: Http,private router: Router) {}
 
 ngOnInit() {
   this.PartnerList =this.http.get('http://localhost:3001/LoadPartners').map(it => it.json());    
-  console.log(this.PartnerList);
 
   this.http.get('http://localhost:3001/LoadPartners').subscribe((res) => {
     const FilesList = res.json();
@@ -56,19 +55,12 @@ ngOnInit() {
   }
 );
 
-  // this.http.get('http://localhost:3001/LoadPartners')
-  //               .toPromise()
-  //               .then(res => this.Car= res.json().data)
-  //               .then(data => { return data; });
 
 }
 
-changeSort(event) {
-  if (!event.order) {
-    this.sortF = 'year';
-  } else {
-    this.sortF = event.field;
-  }
+onRowSelect(event) {
+  this.router.navigate(['nomenclature/partners/partners-details/'+event.data.PartnerId])
+  // console.log(event.data.PartnerId);
 }
 
 PartnerEdit(PartnerId) {
@@ -86,174 +78,3 @@ PartnerDelete(PartnerId) {
 
 }
 }
-
-//   @ViewChild(MatSort) sort: MatSort;
-
-//   displayedColumns = ['PartnerStatus', 'PartnerName',  'PartnerCode', 'PartnerType', 'FiscalRegistration', 'PartnerAddress', 'LegalForm'];
-//   dataSource = new MatTableDataSource(ELEMENT_DATA);
-
-//   applyFilter(filterValue: string) {
-//     filterValue = filterValue.trim(); // Remove whitespace
-//     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-//     this.dataSource.filter = filterValue;
-//   }
-  
-//   // tslint:disable-next-line:use-life-cycle-interface
-//   ngAfterViewInit() {
-//     this.dataSource.sort = this.sort;
-//   }
-//   // tslint:disable-next-line:member-ordering
-//   length = 100;
-//   // tslint:disable-next-line:member-ordering
-//   pageSize = 10;
-//   // tslint:disable-next-line:member-ordering
-//   pageSizeOptions = [5, 10, 25, 100];
-
-//   // MatPaginator Output
-//   // tslint:disable-next-line:member-ordering
-//   pageEvent: PageEvent;
-
-//   setPageSizeOptions(setPageSizeOptionsInput: string) {
-//     this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
-//   }
-
-//   onSelect(PartnerCode) {
-//     this.router.navigate(['nomenclature/partners/partners-details/',PartnerCode]);
-//    }
-
-
-// }
-
-
-// export interface Element {
-
-//   PartnerStatus : string;
-//   PartnerName : string;
-//   PartnerCode : string;
-//   PartnerType : string;
-//   FiscalRegistration : string;
-//   PartnerAddress : string;
-//   LegalForm: string;
-// }
-
-
-// const ELEMENT_DATA: Element[] = [
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Active',
-//     'PartnerName': 'Ornare Tortor At Corp.',
-//     'PartnerCode': '23432',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '23423',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Active',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Active',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Erat Eget Ipsum Corporation',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Convallis PC',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Ornare Tortor At Corp.',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Inactive',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-//   {
-//     'PartnerStatus': 'Active',
-//     'PartnerName': 'Molestie Dapibus Ligula Consulting',
-//     'PartnerCode': '1270',
-//     'PartnerType': 'Client',
-//     'FiscalRegistration': '8021',
-//     'PartnerAddress': 'P.O. Box 797, 9554 Nibh. Rd.',
-//     'LegalForm': 'SRL'
-//   },
-// ]
-  
