efc0a774986918245302903f2a7a9d185a27368a
Razvan Mustata <razvan.mustata@gmail.com>
2018-01-13 17:34:38 +0200

    Adaugat la pagina de articole

--- src/app/nomenclature/items/items.component.ts
+++ src/app/nomenclature/items/items.component.ts
@@ -1,35 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators,FormControl,FormGroup,FormBuilder} from '@angular/forms';
-
+import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ItemsComponent implements OnInit {
   userform: FormGroup;
   submitted: boolean;
   msgs = [];
-  genders = [];
+  ItemType =[];
   description: string;
 
   ItemName;
-   
+  IsValid = [];
+
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
       this.userform = this.fb.group({
-          'denumire': new FormControl('', Validators.required),
-          'lastname': new FormControl('', Validators.required),
-          'password': new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
-          'description': new FormControl(''),
-          'gender': new FormControl('', Validators.required)
+          'ItemName': new FormControl('', Validators.required),
+          'ItemCode': new FormControl('', Validators.required),         
+          'ItemDescription': new FormControl(''),
+          'ItemType': new FormControl('', Validators.required),
+          'IsValid': new FormControl('', Validators.required)
       });
 
-      this.genders = [];
-      this.genders.push({label:'Select Gender', value:''});
-      this.genders.push({label:'Male', value:'Male'});
-      this.genders.push({label:'Female', value:'Female'});
+      this.ItemType = [];
+      this.ItemType.push({label:'Selecteaza Tip Articol', value:''});
+      this.ItemType.push({label:'Servicii', value:'Servicii'});
+      this.ItemType.push({label:'Marfa', value:'Marfa'});
+      this.ItemType.push({label:'Consumabile', value:'Consumabile'});
+      this.IsValid = [];
+      this.IsValid.push({label:'Da', value:'Da'});
+      this.IsValid.push({label:'Nu', value:'Nu'});
+
   }
 
   onSubmit(value: string) {
