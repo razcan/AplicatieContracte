9d06c2919f539c6aa443272e1a3cb68c6026668e
Razvan Mustata <razvan.mustata@gmail.com>
2018-01-18 14:44:19 +0200

    adugare fereastra proprietati

--- src/app/nomenclature/properties/properties.component.ts
+++ src/app/nomenclature/properties/properties.component.ts
@@ -1,32 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Validators,FormControl,FormGroup,FormBuilder} from '@angular/forms';
+import { ViewEncapsulation } from '@angular/core';
+import { Message,MenuItem,TreeNode, Tree} from 'primeng/primeng';
+import { HostListener } from '@angular/core';
+import { Component, Input, Output, AfterViewInit, ElementRef, EventEmitter, OnChanges, OnInit,ViewChild, NgModule } from '@angular/core';
+import { PageEvent} from '@angular/material';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { Http, HttpModule } from '@angular/http';
+import { HttpParams, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import {DialogModule} from 'primeng/primeng';
+import { ActivatedRoute, NavigationEnd, Params, PRIMARY_OUTLET } from "@angular/router";
+import "rxjs/add/operator/filter";
+import {Injectable} from '@angular/core';
+import { SelectItem } from 'primeng/primeng';
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.css']
+  styleUrls: ['./properties.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PropertiesComponent implements OnInit {
 
   cols: any[];
-  cars: any[];
   dialogVisible: boolean;
+  row: any[];
+  PropertyType;
+  property;
+  PropertyTypeNew : SelectItem[];
   constructor() { }
 
   ngOnInit() {
     this.cols = [
-      {field: 'vin', header: 'Vin'},
-      {field: 'year', header: 'Year'},
-      {field: 'brand', header: 'Brand'},
-      {field: 'color', header: 'Color'}
+      {field: 'DenumireProprietate', header: 'DenumireProprietate'},
+      {field: 'CodProprietate', header: 'CodProprietate'},
+      {field: 'TipProprietate', header: 'TipProprietate'}
   ];
 
-this.cars = [
-      {vin: '1', year: '2015', brand :"vw", color: "red"},
-      {vin: '3', year: '2017', brand :"opel", color: "green"},
-      {vin: '5', year: '2018', brand :"mercedes", color: "yellow"},
+
+
+this.property = [
+      {DenumireProprietate: 'Suparafata', CodProprietate: '1', TipProprietate :"Numeric"},
+      {DenumireProprietate: 'Agent', CodProprietate: '2', TipProprietate :"Text"},
+      {DenumireProprietate: 'Data Reziliere', CodProprietate: '3', TipProprietate :"Date"},
 
   ];
+  
   }
+  display: boolean = false;
 
+  showDialog() {
+      this.display = true;
+  }
+SalveazaProprieatte(DenumireProprietate,CodProprietate,TipProprietate){
+  console.log(DenumireProprietate,CodProprietate,TipProprietate);
+  DenumireProprietate='';
+  CodProprietate='';
+  TipProprietate='';
+  this.display=false;
+
+}
 
 }
